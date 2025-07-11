5b4e9241a28b3b8711c88a977dd368a3943f2d6f
ordidxzero <ordidxzero@gmail.com>
2025-07-11 16:56:37 +0900

    Initial Commit

---
+++ llmark/utils/__init__.py
@@ -0,0 +1,159 @@
+import importlib.util
+import os, socket, subprocess
+from importlib.metadata import version
+from typing import Optional, Dict, Literal, TypedDict, List
+from pathlib import Path
+from string import Formatter
+
+# 패키지 버전 정보 확인 방법: https://stackoverflow.com/questions/20180543/how-do-i-check-the-versions-of-python-modules
+def find_package_version(pkg: str):
+    return version(pkg)
+
+# 패키지 설치 여부 확인 방법: https://stackoverflow.com/questions/1051254/check-if-python-package-is-installed
+def find_package(pkg: str, ver: Optional[str] = None, raise_error: bool = True):
+    """패키지가 설치되었는지, 버전이 일치하는지 확인하는 함수"""
+    is_installed = importlib.util.find_spec(pkg)
+
+    if not is_installed:
+        if raise_error:
+            raise ImportError(f"{pkg} is not installed")
+        else:
+            return False
+    
+    if ver is None:
+        return True
+    
+    is_version_matched = find_package_version(pkg) == ver
+
+    if not is_version_matched:
+        if raise_error:
+            raise ImportError(f"{pkg} version is not matched")
+        else:
+            return False
+    
+    return True
+
+class CommandTemplate:
+    _is_valid: bool
+    _skeleton: str
+    _log_dir: Path
+    _log_prefix: str
+    _cmd: str
+    _log_filename: str
+    def __init__(self, template: str, log_prefix: str = ''):
+        self._is_valid = False
+        self._skeleton = template
+        self._log_dir = Path(".")
+        self._cmd = ""
+        self._log_filename = ''
+        self._log_prefix = f"{log_prefix}_" if log_prefix != "" else ""
+
+    def hydrate(self, **kwargs: str | int | float):
+        self._is_valid = True
+        self._log_filename = self._log_prefix
+        # hydrate
+        arg_names = self._get_vars_from_f_string()
+        args: Dict[str, str | int | float] = {}
+
+        for key, value in kwargs.items():
+            if key in arg_names:
+                args[key] = value
+                self._log_filename += f"{key}_{value}"
+        
+        self._log_filename += '.log'
+
+        self._cmd = self._skeleton.format(**args)
+
+
+    def set_log_dir(self, log_dir: Path) -> None:
+        self._log_dir = log_dir
+
+    def exec(self) -> None:
+        assert self._is_valid == True, "You should invoke `hydrate` method"
+
+        cmd = self._transform()
+        os.system(cmd)
+
+    def split(self, sep: str = " ") -> List[str]:
+        cmd = self._transform()
+        return cmd.split(sep)
+
+    def _transform(self):
+        cmd = self._cmd
+
+        if self._log_filename != "":
+            log_path = self._log_dir / self._log_filename
+            log_path = log_path.absolute().as_posix()
+
+            cmd += " >> " + log_path
+
+        return cmd
+    
+    def _get_vars_from_f_string(self):
+        """f-string에서 변수 이름을 추출하는 메서드"""
+        return [fn for _, fn, _, _ in Formatter().parse(self._skeleton) if fn is not None]
+
+RunnerType = Literal["vllm", "tensorrt_llm", "triton", "vllm_rbln"]
+ENV = Dict[str, str]
+
+class BenchmarkArgs(TypedDict):
+    log_dir: Path
+    log_prefix: str
+    envs: ENV | None
+    
+class Benchmark:
+    runner_type: Optional[RunnerType]
+    _env: Dict[str, str]
+    _log_dir: Path
+    _log_prefix: str
+    _log_path: Dict[str, Path]
+    _cmd: Dict[str, CommandTemplate]
+
+    def __init__(self, log_dir: Path = Path("."), log_prefix: str = '', envs: ENV | None = None):
+        # BenchmarkRunner Type
+        self.runner_type = None
+
+        # Environment Variables 관리
+        self._env = os.environ.copy()
+        self._env["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+        if envs is not None:
+            assert isinstance(envs, dict), "env must be dictionary"
+            self._env.update(envs)
+
+        # Path 관리
+        self._log_dir = log_dir
+        self._log_prefix = log_prefix
+        self._log_path = {}
+
+        # Command 관리
+        self._skeleton_cmd = {}
+        self._cmd = {}
+    
+    def _set_runner_type(self, runner_type: RunnerType):
+        """Runner Type을 지정하는 메서드. __init__ 메서드에서 반드시 호출되어야 한다"""
+        assert runner_type in ["vllm", "tensorrt_llm", "triton", "rbln"], "Invalid RunnerType"
+        self.runner_type = runner_type
+
+    @staticmethod
+    def is_port_open(host:str  = "localhost", port: int = 8000) -> bool:
+        """포트가 열려 있는지 확인하는 메서드"""
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.settimeout(1)
+            result = sock.connect_ex((host, port))
+            return result == 0
+    
+    @staticmethod
+    def is_docker() -> bool:
+        """Docker 환경인지 체크하는 메서드"""
+        cgroup = Path('/proc/self/cgroup')
+        return Path('/.dockerenv').is_file() or (cgroup.is_file() and 'docker' in cgroup.read_text())
+
+    @staticmethod
+    def is_rbln() -> bool:
+        """Rebellions NPU 환경을 지원하는지 체크하는 메서드"""
+        try:
+            subprocess.check_output("rbln-stat")
+            return True
+        except Exception:
+            return False
+        
---
+++ llmark/vllm/__init__.py
@@ -0,0 +1,72 @@
+import subprocess, time
+from typing_extensions import Unpack, Callable
+from llmark.utils import find_package, find_package_version, Benchmark, BenchmarkArgs, CommandTemplate
+
+class VLLMBenchmarkRunner(Benchmark):
+    _server_process: subprocess.Popen[str] | None
+    _terminate_server: Callable[..., None] | None
+    _is_init: bool
+    def __init__(self, benchmark_cmd: str, server_cmd: str, **kwargs: Unpack[BenchmarkArgs]):
+        super().__init__(**kwargs)
+
+        self._set_runner_type("vllm")
+
+        self._server_process = None
+        self._terminate_server = None
+
+        self._cmd["benchmark"] = CommandTemplate(benchmark_cmd)
+        self._cmd["server"] = CommandTemplate(server_cmd)
+
+        self._is_init = False
+    
+    def init(self, **kwargs: str | int | float):
+        self._cmd["benchmark"].set_log_dir(self._log_dir / "client")
+        self._cmd["server"].set_log_dir(self._log_dir / "server")
+
+        for _, cmd in self._cmd.items():
+            cmd.hydrate(**kwargs)
+
+        self._is_init = True
+
+    def run_server(self):
+        assert self._is_init == True, "Runner is not initalized. Invoke init()"
+        assert Benchmark.is_port_open() == False, "Port 8000 is already used."
+        if self._server_process is not None:
+            assert self._server_process.poll() == 0, "Server does not closed successfully."
+            self._server_process = None
+        
+        print("Running...")
+        log_file = open(self._log_path["server"], "w")
+        
+        server_process = subprocess.Popen(args=self._cmd["server"].split(" "), text=True, encoding="utf-8", stdout=log_file, env=self._env)
+        self._server_process = server_process
+
+        def terminate():
+            log_file.close()
+            server_process.terminate()
+            server_process.wait(timeout=60)
+            print("Terminated...")
+
+        self._terminate_server = terminate
+
+    def run_benchmark(self):
+        print("Start Benchmark...")
+        self._cmd['benchmark'].exec()
+
+    def run(self, **kwargs: str | int | float):
+        """init, run_server, run_benchmark를 합친 메서드"""
+        self.init(**kwargs)
+        self.run_server()
+
+        assert self._terminate_server != None, "Server is not initialized"
+
+        while True:
+            if Benchmark.is_port_open():
+                self.run_benchmark()
+                self._terminate_server()
+                break
+            time.sleep(1)
+
+if __name__ != "__main__":
+    find_package("vllm")
+    print("Current vLLM Version: ", find_package_version('vllm'))
