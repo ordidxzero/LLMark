2aadd42605a6898bd5b3272b67b49c4b2a9fe654
ordidxzero <ordidxzero@gmail.com>
2025-07-12 17:31:57 +0900

    update

--- llmark/vllm/__init__.py
+++ llmark/vllm/__init__.py
@@ -1,12 +1,26 @@
 import subprocess, time
 from typing_extensions import Unpack, Callable
-from llmark.utils import find_package, find_package_version, Benchmark, BenchmarkArgs, CommandTemplate
+from llmark.utils import (
+    find_package,
+    find_package_version,
+    Benchmark,
+    BenchmarkArgs,
+    CommandTemplate,
+)
+
 
 class VLLMBenchmarkRunner(Benchmark):
     _server_process: subprocess.Popen[str] | None
     _terminate_server: Callable[..., None] | None
     _is_init: bool
-    def __init__(self, benchmark_cmd: str, server_cmd: str, log_prefix: str = '', **kwargs: Unpack[BenchmarkArgs]):
+
+    def __init__(
+        self,
+        benchmark_cmd: str,
+        server_cmd: str,
+        log_prefix: str = "",
+        **kwargs: Unpack[BenchmarkArgs]
+    ):
         super().__init__(**kwargs)
 
         self._set_runner_type("vllm")
@@ -19,7 +33,7 @@ class VLLMBenchmarkRunner(Benchmark):
         self._cmd["server"] = CommandTemplate(server_cmd, stdout_log=True)
         self._cmd["server"].set_log_prefix(log_prefix)
         self._is_init = False
-    
+
     def init(self, **kwargs: str | int | float):
         self._cmd["benchmark"].set_log_dir(self._log_dir / "client")
         self._cmd["server"].set_log_dir(self._log_dir / "server")
@@ -41,13 +55,21 @@ class VLLMBenchmarkRunner(Benchmark):
         assert self._is_init == True, "Runner is not initalized. Invoke init()"
         assert Benchmark.is_port_open() == False, "Port 8000 is already used."
         if self._server_process is not None:
-            assert self._server_process.poll() == 0, "Server does not closed successfully."
+            assert (
+                self._server_process.poll() == 0
+            ), "Server does not closed successfully."
             self._server_process = None
-        
+
         print("Running...")
-        log_file = open(self._cmd['server'].get_absolute_log_path(), "w")
-        
-        server_process = subprocess.Popen(args=self._cmd["server"].split(" "), text=True, encoding="utf-8", stdout=log_file, env=self._env)
+        log_file = open(self._cmd["server"].get_absolute_log_path(), "w")
+
+        server_process = subprocess.Popen(
+            args=self._cmd["server"].split(" "),
+            text=True,
+            encoding="utf-8",
+            stdout=log_file,
+            env=self._env,
+        )
         self._server_process = server_process
 
         def terminate():
@@ -60,7 +82,8 @@ class VLLMBenchmarkRunner(Benchmark):
 
     def run_benchmark(self):
         print("Start Benchmark...")
-        self._cmd['benchmark'].exec()
+        self._cmd["benchmark"].set_env(self._env)
+        self._cmd["benchmark"].exec()
 
     def run(self, **kwargs: str | int | float):
         """init, run_server, run_benchmark를 합친 메서드"""
@@ -76,6 +99,7 @@ class VLLMBenchmarkRunner(Benchmark):
                 break
             time.sleep(1)
 
+
 if __name__ != "__main__":
     find_package("vllm")
-    print("Current vLLM Version: ", find_package_version('vllm'))
+    print("Current vLLM Version: ", find_package_version("vllm"))
--- main.py
+++ main.py
@@ -2,37 +2,51 @@ from llmark.vllm import VLLMBenchmarkRunner
 from pathlib import Path
 from tqdm import tqdm
 
-BENCHMARK_CMD = "uv run _vllm/benchmarks/benchmark_serving.py --backend vllm --model meta-llama/Llama-3.1-8B-Instruct --dataset-name random --random-input-len {input_len} --ignore-eos --random-output-len {output_len} --num-prompts 1024"
-SERVER_CMD = "vllm serve meta-llama/Llama-3.1-8B-Instruct --dtype bfloat16 --disable-log-requests"
-runner = VLLMBenchmarkRunner(benchmark_cmd=BENCHMARK_CMD, server_cmd=SERVER_CMD, log_dir=Path("./output"))
+BENCHMARK_CMD = "uv run _vllm/benchmarks/benchmark_serving.py --backend vllm --model meta-llama/Meta-Llama-3-8B --dataset-name random --random-input-len {input_len} --ignore-eos --random-output-len {output_len} --num-prompts 4096"
+SERVER_CMD = (
+    "vllm serve meta-llama/Meta-Llama-3-8B --dtype bfloat16 --disable-log-requests"
+)
+runner = VLLMBenchmarkRunner(
+    benchmark_cmd=BENCHMARK_CMD, server_cmd=SERVER_CMD, log_dir=Path("./output")
+)
 
 INPUT_LENGTHS = [2048, 128]
 OUTPUT_LENGTHS = [2048, 128]
 MAX_BATCH_SIZES = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]
 REQUEST_RATES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-pbar = tqdm(total= len(OUTPUT_LENGTHS) * len(INPUT_LENGTHS) + len(MAX_BATCH_SIZES) + len(REQUEST_RATES))
+pbar = tqdm(
+    total=len(OUTPUT_LENGTHS) * len(INPUT_LENGTHS)
+    + len(MAX_BATCH_SIZES)
+    + len(REQUEST_RATES)
+)
 
 for output_len in OUTPUT_LENGTHS:
     for input_len in INPUT_LENGTHS:
-        runner.set_log_prefix("exp1")
+        runner.set_log_prefix("A100-40GB_exp1")
         runner.run(input_len=input_len, output_len=output_len)
         pbar.update(1)
 
-BENCHMARK_CMD_2 = "uv run _vllm/benchmarks/benchmark_serving.py --backend vllm --model meta-llama/Llama-3.1-8B-Instruct --dataset-name random --random-input-len {input_len} --ignore-eos --random-output-len {output_len} --num-prompts 4096"
-SERVER_CMD_2 = "vllm serve meta-llama/Llama-3.1-8B-Instruct --dtype bfloat16 --disable-log-requests --max-num-seqs {max_num_seqs}"
-runner_2 = VLLMBenchmarkRunner(benchmark_cmd=BENCHMARK_CMD_2, server_cmd=SERVER_CMD_2, log_dir=Path("./output"))
+BENCHMARK_CMD_2 = "uv run _vllm/benchmarks/benchmark_serving.py --backend vllm --model meta-llama/Meta-Llama-3-8B --dataset-name random --random-input-len {input_len} --ignore-eos --random-output-len {output_len} --num-prompts 4096"
+SERVER_CMD_2 = "vllm serve meta-llama/Meta-Llama-3-8B --dtype bfloat16 --disable-log-requests --max-num-seqs {max_num_seqs}"
+runner_2 = VLLMBenchmarkRunner(
+    benchmark_cmd=BENCHMARK_CMD_2, server_cmd=SERVER_CMD_2, log_dir=Path("./output")
+)
 
 for max_num_seqs in MAX_BATCH_SIZES:
-    runner_2.set_log_prefix("exp2")
+    runner_2.set_log_prefix("A100-40GB_exp2")
     runner_2.run(max_num_seqs=max_num_seqs, input_len=2048, output_len=128)
     pbar.update(1)
 
-BENCHMARK_CMD_3 = "uv run _vllm/benchmarks/benchmark_serving.py --backend vllm --model meta-llama/Llama-3.1-8B-Instruct --dataset-name random --random-input-len {input_len} --ignore-eos --random-output-len {output_len} --num-prompts 512 --request-rate {request_rate}"
-SERVER_CMD_3 = "vllm serve meta-llama/Llama-3.1-8B-Instruct --dtype bfloat16 --disable-log-requests"
-runner_3 = VLLMBenchmarkRunner(benchmark_cmd=BENCHMARK_CMD_3, server_cmd=SERVER_CMD_3, log_dir=Path("./output"))
+BENCHMARK_CMD_3 = "uv run _vllm/benchmarks/benchmark_serving.py --backend vllm --model meta-llama/Meta-Llama-3-8B --dataset-name random --random-input-len {input_len} --ignore-eos --random-output-len {output_len} --num-prompts 512 --request-rate {request_rate}"
+SERVER_CMD_3 = (
+    "vllm serve meta-llama/Meta-Llama-3-8B --dtype bfloat16 --disable-log-requests"
+)
+runner_3 = VLLMBenchmarkRunner(
+    benchmark_cmd=BENCHMARK_CMD_3, server_cmd=SERVER_CMD_3, log_dir=Path("./output")
+)
 
 for request_rate in REQUEST_RATES:
-    runner_3.set_log_prefix("exp3")
+    runner_3.set_log_prefix("A100-40GB_exp3")
     runner_3.run(request_rate=request_rate, input_len=2048, output_len=128)
     pbar.update(1)
