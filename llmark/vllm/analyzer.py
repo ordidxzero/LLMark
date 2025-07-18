20f8170cf41196e7fb7443360d940005c9c4bcd9
YUJUN HEO <ordidxzero@gmail.com>
2025-07-18 20:23:35 +0900

    test

--- llmark/vllm/analyzer.py
+++ llmark/vllm/analyzer.py
@@ -9,22 +9,28 @@ class VLLMAnalyzer(LogAnalyzer):
         super().__init__(log_dir)
     
     def _parse_line(self, line: str):
-        result = re.search(r'iter: (\d+) Avg prompt throughput: (\d+.\d+) tokens/s, Avg generation throughput: (\d+.\d+) tokens/s, Running: (\d+) reqs, Swapped: (\d+) reqs, Pending: (\d+) reqs, GPU KV cache usage: (\d+.\d+)%, CPU KV cache usage: (\d+.\d+)%.Prompt stage: (-?\d+) reqs, Generation stage: (-?\d+) reqs, Preemption: (\d+) reqsLatenyMS: (\d+.\d+)', line)
+        result = re.search(r'iter: (\d+), Avg prompt throughput: (\d+.\d+) tokens/s, Avg generation throughput: (\d+.\d+) tokens/s, Running: (\d+) reqs, Swapped: (\d+) reqs, Pending: (\d+) reqs, GPU KV cache usage: (\d+.\d+)%, CPU KV cache usage: (\d+.\d+)%, GPU Total Block: (\d+), GPU Free Block: (\d+), CPU Total Block: (\d+), CPU Free Block: (\d+), actual_num_batched_tokens: (\d+), Prompt stage: (\d+) reqs, Generation stage: (\d+) reqs, Preemption: (\d+) reqs, Generation: (\d+) tokens, Latency: (\d+.\d+)', line)
         if result is None:
             breakpoint()
         
         iter_num = int(result.group(1))
         prompt_throughput = float(result.group(2))
         generation_throughput = float(result.group(3))
-        running_seqs = int(result.group(4))
+        # running_seqs = int(result.group(4))
         # swapped_seqs = int(result.group(5))
         # pending_seqs = int(result.group(6))
-        prompt_seqs = int(result.group(9))
-        generation_seqs = int(result.group(10))
-        preemption_seqs = int(result.group(11))
-        latency_iter = float(result.group(12))
+        gpu_total_block = int(result.group(9))
+        gpu_free_block = int(result.group(10))
+        cpu_total_block = int(result.group(11))
+        cpu_free_block = int(result.group(12))
+        actual_num_batched_tokens = int(result.group(13))
+        prompt_seqs = int(result.group(14))
+        generation_seqs = int(result.group(15))
+        preemption_seqs = int(result.group(16))
+        generation_tokens = int(result.group(17))
+        latency_iter = float(result.group(18))
 
-        return iter_num, prompt_throughput, generation_throughput, running_seqs, prompt_seqs, generation_seqs, preemption_seqs, latency_iter
+        return iter_num, prompt_throughput, generation_throughput, prompt_seqs, generation_seqs, preemption_seqs, latency_iter
     
     def _get_prompt_iter_gap(self, iter_nums: List[int]):
         gaps = []
@@ -34,6 +40,9 @@ class VLLMAnalyzer(LogAnalyzer):
             gaps.append(cur_iter - prev_iter)
 
         return gaps
+    
+    def _mean(self, d: List[int | float]):
+        return round(sum(d) / len(d), 2)
 
     def run(self, pattern: str, save_dir: Path = None):
         assert pattern is not None and pattern != "" and isinstance(pattern, str), "Error"
@@ -43,13 +52,6 @@ class VLLMAnalyzer(LogAnalyzer):
                 "iter_nums": [],
                 "running_seqs": [],
                 "latencies": [],
-                "group_latencies": [],
-                "buffer": [],
-                "group_buffer": []
-            }
-            prompt_fsm = {
-                "flag": 0, # 0이면 초기화 상태, 1이면 prompt start 
-                "type": 0 #  0이면 초기화 상태, 1이면 Prompt 한번, 2이면 Prompt 연속적으로 여러번
             }
             generation_state = {
                 "iter_nums": [],
@@ -62,54 +64,24 @@ class VLLMAnalyzer(LogAnalyzer):
             with open(logfile_path, "r") as logfile:
                 for line in logfile:
                     if "metrics.py" in line:
-                        iter_num, prompt_throughput, generation_throughput, running_seqs, prompt_seqs, generation_seqs, preemption_seqs, latency = self._parse_line(line)
+                        iter_num, prompt_throughput, generation_throughput, prompt_seqs, generation_seqs, preemption_seqs, latency = self._parse_line(line)
                         rest_state['preemption_seqs'].append(preemption_seqs)
-                        if prompt_throughput == 0.0:
-                            if prompt_seqs == 0 and prompt_fsm['flag'] == 1:
-                                # Prompt stage가 끝난 직후
-                                if prompt_fsm['type'] == 1:
-                                    buffer = prompt_state['buffer']
-                                    prompt_state['iter_nums'].append(buffer[0])
-                                    prompt_state['running_seqs'].append(buffer[1])
-                                    prompt_state['latencies'].append(buffer[2])
-                                elif prompt_fsm['type'] == 2:
-                                    group_buffer = prompt_state['group_buffer']
-                                    prompt_state['group_latencies'].append(sum(group_buffer))
-                                    prompt_state['iter_nums'].append(iter_num - int(group_buffer / 2))
-
-                                prompt_fsm = {"flag": 0, "type": 0}
-                                prompt_state["buffer"] = []
-                                prompt_state['group_buffer'] = []
-                            
-                            if prompt_seqs != 0:
-                                prompt_fsm['flag'] = 1
-                            
-                        if prompt_throughput != 0.0 and prompt_fsm['flag'] == 1:
-                            prompt_state['group_buffer'].append(latency)
-                            prompt_state['running_seqs'].append(prompt_seqs)
-                            if prompt_fsm['type'] == 0:
-                                prompt_state['buffer'] = [iter_num, prompt_seqs, latency]
-                                prompt_fsm['type'] == 1
-                            elif prompt_fsm['type'] == 1:
-                                prompt_fsm['type'] == 2
-                            else:
-                                continue
                         
-                        if prompt_throughput == 0.0 and prompt_fsm['flag'] == 0:
-                            generation_state['iter_nums'].append(iter_num)
+                        if prompt_throughput != 0.0:
+                            # Prompt Stage
+                            prompt_state["iter_nums"].append(iter_num)
+                            prompt_state['running_seqs'].append(prompt_seqs)
+                            prompt_state['latencies'].append(latency)
+                        elif generation_throughput != 0.0:
+                            # Generation Stage
+                            generation_state["iter_nums"].append(iter_num)
                             generation_state['running_seqs'].append(generation_seqs)
                             generation_state['latencies'].append(latency)
-            
+                        else:
+                            continue
+
             all_running_seqs = generation_state['running_seqs'] + prompt_state['running_seqs']
             prompt_iter_gaps = self._get_prompt_iter_gap(prompt_state['iter_nums'])
-            prompt_seqs_list = prompt_state['running_seqs']
-            prompt_latencies = prompt_state['latencies']
-            generation_seqs_list = generation_state['running_seqs']
-            generation_latencies = generation_state['latencies']
-            prompt_group_latencies = prompt_state['group_latencies']
-
-            if len(prompt_latencies) == 0:
-                breakpoint()
 
 
             dist_dir = save_dir if save_dir is not None else self._log_dir
@@ -119,13 +91,12 @@ class VLLMAnalyzer(LogAnalyzer):
 
             with open(dist_dir / f"{logfile_path.stem}.json", "w") as output_file:
                 content = {
-                    "avg_running_seqs": round(sum(all_running_seqs) / len(all_running_seqs), 2),
-                    "avg_prompt_seqs": round(sum(prompt_seqs_list) / len(prompt_seqs_list), 2),
-                    "avg_generation_seqs": round(sum(generation_seqs_list) / len(generation_seqs_list), 2),
-                    "avg_prompt_latency": round(sum(prompt_latencies) / len(prompt_latencies), 2),
-                    "avg_generation_latency": round(sum(generation_latencies) / len(generation_latencies), 2),
-                    "avg_prompt_group_latency": round(sum(prompt_group_latencies) / len(prompt_group_latencies), 2),
-                    "prefill_frequency": round(sum(prompt_iter_gaps) / len(prompt_iter_gaps), 2)
+                    "avg_running_seqs": self._mean(all_running_seqs),
+                    "avg_prompt_seqs": self._mean(prompt_state['running_seqs']),
+                    "avg_generation_seqs": self._mean(generation_state['running_seqs']),
+                    "avg_prompt_latency": self._mean(prompt_state['latencies']),
+                    "avg_generation_latency": self._mean(generation_state['latencies']),
+                    "prefill_frequency": self._mean(prompt_iter_gaps)
                 }
 
                 json.dump(content, output_file, indent=2)
--- main.py
+++ main.py
@@ -2,34 +2,34 @@ from llmark.vllm import VLLMBenchmarkRunner
 from pathlib import Path
 from tqdm import tqdm
 
-benchmark_cmd = "uv run _vllm/benchmarks/benchmark_serving.py --backend vllm --model meta-llama/Meta-Llama-3-8B --dataset-name random --random-input-len {input_len} --ignore-eos --random-output-len {output_len} --num-prompts 4096"
-server_cmd = "vllm serve meta-llama/Meta-Llama-3-8B --dtype bfloat16 --disable-log-requests"
-runner = VLLMBenchmarkRunner(benchmark_cmd=benchmark_cmd, server_cmd=server_cmd, log_dir=Path("./output"))
-for output_len in [2048, 128]:
-    for input_len in [2048, 128]:
-        runner.set_log_prefix("A100-80GB_exp1")
-        runner.run(input_len=input_len, output_len=output_len)
+# benchmark_cmd = "uv run _vllm/benchmarks/benchmark_serving.py --backend vllm --model meta-llama/Meta-Llama-3-8B --dataset-name random --random-input-len {input_len} --ignore-eos --random-output-len {output_len} --num-prompts 4096"
+# server_cmd = "vllm serve meta-llama/Meta-Llama-3-8B --dtype bfloat16 --disable-log-requests"
+# runner = VLLMBenchmarkRunner(benchmark_cmd=benchmark_cmd, server_cmd=server_cmd, log_dir=Path("./output"))
+# for output_len in [2048, 128]:
+#     for input_len in [2048, 128]:
+#         runner.set_log_prefix("A100-80GB_exp1")
+#         runner.run(input_len=input_len, output_len=output_len)
 
-benchmark_cmd = "uv run _vllm/benchmarks/benchmark_serving.py --backend vllm --model meta-llama/Meta-Llama-3-8B --dataset-name random --random-input-len {input_len} --ignore-eos --random-output-len {output_len} --num-prompts 4096"
-server_cmd = "vllm serve meta-llama/Meta-Llama-3-8B --dtype bfloat16 --disable-log-requests --max-num-seqs {max_num_seqs}"
-runner = VLLMBenchmarkRunner(benchmark_cmd=benchmark_cmd, server_cmd=server_cmd, log_dir=Path("./output"))
-for max_num_seqs in [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]:
-    runner.set_log_prefix("A100-80GB_exp2")
-    runner.run(max_num_seqs=max_num_seqs, input_len=2048, output_len=128)
+# benchmark_cmd = "uv run _vllm/benchmarks/benchmark_serving.py --backend vllm --model meta-llama/Meta-Llama-3-8B --dataset-name random --random-input-len {input_len} --ignore-eos --random-output-len {output_len} --num-prompts 4096"
+# server_cmd = "vllm serve meta-llama/Meta-Llama-3-8B --dtype bfloat16 --disable-log-requests --max-num-seqs {max_num_seqs}"
+# runner = VLLMBenchmarkRunner(benchmark_cmd=benchmark_cmd, server_cmd=server_cmd, log_dir=Path("./output"))
+# for max_num_seqs in [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]:
+#     runner.set_log_prefix("A100-80GB_exp2")
+#     runner.run(max_num_seqs=max_num_seqs, input_len=2048, output_len=128)
 
-benchmark_cmd = "uv run _vllm/benchmarks/benchmark_serving.py --backend vllm --model meta-llama/Meta-Llama-3-8B --dataset-name random --random-input-len {input_len} --ignore-eos --random-output-len {output_len} --num-prompts 512 --request-rate {request_rate}"
-server_cmd = "vllm serve meta-llama/Meta-Llama-3-8B --dtype bfloat16 --disable-log-requests"
-runner = VLLMBenchmarkRunner(benchmark_cmd=benchmark_cmd, server_cmd=server_cmd, log_dir=Path("./output"))
-for request_rate in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
-    runner.set_log_prefix("A100-80GB_exp3")
-    runner.run(request_rate=request_rate, input_len=2048, output_len=128)
+# benchmark_cmd = "uv run _vllm/benchmarks/benchmark_serving.py --backend vllm --model meta-llama/Meta-Llama-3-8B --dataset-name random --random-input-len {input_len} --ignore-eos --random-output-len {output_len} --num-prompts 512 --request-rate {request_rate}"
+# server_cmd = "vllm serve meta-llama/Meta-Llama-3-8B --dtype bfloat16 --disable-log-requests"
+# runner = VLLMBenchmarkRunner(benchmark_cmd=benchmark_cmd, server_cmd=server_cmd, log_dir=Path("./output"))
+# for request_rate in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
+#     runner.set_log_prefix("A100-80GB_exp3")
+#     runner.run(request_rate=request_rate, input_len=2048, output_len=128)
 
 
 MAX_BATCH_SIZES = [4, 8, 16, 32, 64, 128, 256, 512]
-MAX_NUM_OF_TOKENS = [256, 512, 1024, 2048, 4096, 8192, 16384]
+MAX_NUM_OF_TOKENS = [1024, 2048, 4096, 8192, 16384]
 
 BENCHMARK_CMD= "uv run _vllm/benchmarks/benchmark_serving.py --backend vllm --model meta-llama/Meta-Llama-3-8B --dataset-name random --random-input-len {input_len} --ignore-eos --random-output-len {output_len} --num-prompts 1024"
-SERVER_CMD = "vllm serve meta-llama/Meta-Llama-3-8B --dtype bfloat16 --disable-log-requests --max-num-seqs {max_num_seqs}"
+SERVER_CMD = "vllm serve meta-llama/Meta-Llama-3-8B --dtype bfloat16 --disable-log-requests --max-num-seqs {max_num_seqs} --max-num-batched-tokens {max_num_batched_tokens} --max-model-len 1024"
 runner = VLLMBenchmarkRunner(
     benchmark_cmd=BENCHMARK_CMD, server_cmd=SERVER_CMD, log_dir=Path("./output"), envs={"VLLM_ALLOW_LONG_MAX_MODEL_LEN": "1"}
 )
